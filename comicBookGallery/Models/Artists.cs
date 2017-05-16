d38e7ebbac3c1266d58f79b3b32768ce5cb1dd25
ADMIN <ADMIN@USER>
2017-05-16 07:12:19 +0200

    Added comic book and artists model

---
+++ comicBookGallery/Models/Artists.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace comicBookGallery.Models
+{
+    public class Artists
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
