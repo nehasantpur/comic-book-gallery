11ef5a81eb3ee24be6fe9b76087e434d600a79ad
ADMIN <ADMIN@USER>
2017-05-16 13:46:23 +0200

    Updated comic books list view

--- comicBookGallery/Data/ComicBookRepository.cs
+++ comicBookGallery/Data/ComicBookRepository.cs
@@ -26,25 +26,10 @@ namespace comicBookGallery.Data
                }
 
             },
-             new ComicBook()
-            {
-                Id = 2,
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>",
-                Artists = new Artists[]
-                {
-                    new Artists() { Name = "Dan Slott", Role = "Script" },
-                    new Artists() { Name = "Humberto Ramos", Role = "Pencils" },
-                    new Artists() { Name = "Victor Olazaba", Role = "Inks" },
-                    new Artists() { Name = "Edgar Delgado", Role = "Colors" },
-                    new Artists() { Name = "Chris Eliopoulos", Role = "Letters" },
-                },
-                Favorite = false
-            },
+            
             new ComicBook()
             {
-                Id =3,
+                Id =2,
                 SeriesTitle = "The Amazing Spider-Man",
                 IssueNumber = 657,
                 DescriptionHtml = "<p><strong>FF: THREE TIE-IN.</strong> Spider-Man visits the FF for a very private wake--just for family.</p>",
@@ -60,7 +45,7 @@ namespace comicBookGallery.Data
             },
             new ComicBook()
             {
-                Id=4,
+                Id=3,
                 SeriesTitle = "Bone",
                 IssueNumber = 50,
                 DescriptionHtml = "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious \"Crown of Horns\".</p>",
